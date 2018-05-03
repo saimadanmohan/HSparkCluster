24027f05b8fea567bf4e61cc254436651335a92e
root <root@cp-2.amsg2.ams560-sbu-pg0.utah.cloudlab.us>
2018-05-03 05:05:02 -0600

    adding keys to user

--- keys.sh
+++ keys.sh
@@ -1,5 +1,5 @@
-cat ~/.ssh/ams.pub >> ~/.ssh/authorized_keys;		 
-chmod 0600 ~/.ssh/ams*; 
-chmod 0600 ~/.ssh/authorized_keys; 		 
-rm -rf ~/.ssh/known_hosts;  
-echo 'StrictHostKeyChecking no' >> ~/.ssh/config
+cat /users/Ignite/.ssh/ams.pub >> /users/Ignite/.ssh/authorized_keys;		 
+chmod 0600 /users/Ignite/.ssh/ams*; 
+chmod 0600 /users/Ignite/.ssh/authorized_keys; 		 
+rm -rf /users/Ignite/.ssh/known_hosts;  
+echo 'StrictHostKeyChecking no' >> /users/Ignite/.ssh/config
