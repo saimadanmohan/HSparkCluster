32e817eb2f63e1dec069974a73d443b748c704c4
root <root@ms1040.utah.cloudlab.us>
2018-05-03 06:00:45 -0600

    keys

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-/tmp/master-node.sh
+cp /tmp/id_rsa ~/.ssh/
+cp /tmp/id_rsa.pub ~/.ssh/
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys;
+echo 'StrictHostKeyChecking no' >> ~/.ssh/config
+#/tmp/master-node.sh
 tail -f /dev/null
