6aaede2d04593b7a0ffb677650a64ac0379de3c2
root <root@ms1040.utah.cloudlab.us>
2018-05-03 06:25:30 -0600

    keys in docker

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cp /tmp/id_rsa ~/.ssh/
-cp /tmp/id_rsa.pub ~/.ssh/
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys;
-echo 'StrictHostKeyChecking no' >> ~/.ssh/config
+mkdir -p ~/.ssh
+cp /tmp/id_rsa ~/.ssh/id_rsa
+cp /tmp/id_rsa.pub ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys;
+echo 'StrictHostKeyChecking no' > ~/.ssh/config
 #/tmp/master-node.sh
 tail -f /dev/null
