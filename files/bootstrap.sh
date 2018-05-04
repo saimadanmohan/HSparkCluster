47de2e39773cf84fd2eaa9d053ba9f18bb441dd3
root <root@ms1040.utah.cloudlab.us>
2018-05-03 23:12:48 -0600

    added master and slave dockers

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -4,5 +4,6 @@ cp /tmp/id_rsa ~/.ssh/id_rsa
 cp /tmp/id_rsa.pub ~/.ssh/id_rsa.pub
 cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys;
 echo 'StrictHostKeyChecking no' > ~/.ssh/config
+service ssh start
 /tmp/master-node.sh
 tail -f /dev/null
