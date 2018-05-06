8b770ff9d1427cae9cf09f60ce2fae482c6fc879
root <root@hp192.utah.cloudlab.us>
2018-05-06 00:03:11 -0600

    keys permissions

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -2,6 +2,8 @@
 mkdir -p ~/.ssh
 cp /tmp/id_rsa ~/.ssh/id_rsa
 cp /tmp/id_rsa.pub ~/.ssh/id_rsa.pub
+chmod +x ~/.ssh/id_rsa.pub
+chmod +x ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys;
 echo 'StrictHostKeyChecking no' > ~/.ssh/config
 service ssh start
