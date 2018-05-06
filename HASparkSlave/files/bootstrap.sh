c9d29575711ba1046747e6dec4f17a965ee6b70f
root <root@cp-2.amsg2.ams560-sbu-pg0.utah.cloudlab.us>
2018-05-06 08:53:46 -0600

    spark commented at slaves

--- HASparkSlave/files/bootstrap.sh
+++ HASparkSlave/files/bootstrap.sh
@@ -8,5 +8,5 @@ cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys;
 echo 'StrictHostKeyChecking no' > ~/.ssh/config
 service ssh start
 #/tmp/master-node.sh
-/tmp/spark-setup.sh
+#/tmp/spark-setup.sh
 tail -f /dev/null
